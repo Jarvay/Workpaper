c4005b6349263824ef38345f2d8958b764634aaf
JiaWei <jarvay@163.com>
2024-04-29 10:23:29 +0800

    fix: reset schedule after weekdays changed

--- src/services/weekday.ts
+++ src/services/weekday.ts
@@ -1,7 +1,14 @@
 import { Weekday } from '../../cross/interface';
 import { BaseService } from '@/services/base';
+import { ipcRenderer } from 'electron';
+import { Events } from '../../cross/enums';
 
 export class WeekdayService extends BaseService<'weekdays', Weekday> {
+  async save(list: Weekday[]): Promise<void> {
+    await super.save(list);
+    await ipcRenderer.invoke(Events.ResetSchedule);
+  }
+
   getKeyInDB(): 'weekdays' {
     return 'weekdays';
   }
